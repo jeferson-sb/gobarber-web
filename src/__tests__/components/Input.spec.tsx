5f706c0a099e29a80c92ffba5f24356498f39d15
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-09-23 20:34:49 -0300

    feat: add input specs

---
+++ src/__tests__/components/Input.spec.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, wait, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Input component', () => {
+  it('should be able to render an input', () => {
+    render(<Input name="email" placeholder="E-mail" />);
+    expect(screen.getByPlaceholderText('E-mail')).toBeTruthy;
+  });
+
+  it('should render highlight on input focus', async () => {
+    render(<Input name="email" placeholder="E-mail" />);
+    const inputElement = screen.getByPlaceholderText('E-mail');
+    const containerElement = screen.getByTestId('input-container');
+
+    inputElement.focus();
+
+    await wait(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000');
+      expect(containerElement).toHaveStyle('color: #ff9000');
+    });
+
+    inputElement.blur();
+
+    await wait(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #ff9000');
+      expect(containerElement).not.toHaveStyle('color: #ff9000');
+    });
+  });
+
+  it('should keep input border highlighted when filled', async () => {
+    render(<Input name="email" placeholder="E-mail" />);
+    const inputElement = screen.getByPlaceholderText('E-mail');
+    const containerElement = screen.getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: { value: 'johndoe@example.com.br' },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await wait(() => {
+      expect(containerElement).toHaveStyle('color: #ff9000');
+    });
+  });
+});
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -26,7 +26,7 @@ const Input: React.FC<InputProps> = ({
   const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
-  const {fieldName, defaultValue, error, registerField,} = useField(name);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -47,6 +47,7 @@ const Input: React.FC<InputProps> = ({
       isFocused={isFocused}
       isFilled={isFilled}
       style={containerStyle}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
