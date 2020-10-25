37f57a569c80768c4c89c79f3c1d659540cfdc47
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-10-24 21:53:20 -0300

    refactor: add animations keyframes

---
+++ src/styles/animations.ts
@@ -0,0 +1,25 @@
+import { keyframes } from 'styled-components';
+
+export const slideLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const slideRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
